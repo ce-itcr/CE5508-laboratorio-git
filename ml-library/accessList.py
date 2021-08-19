5c3f4cc856989019fceb916f236693dfa6aaf92a
angelortizv <angelo2898@gmail.com>
2021-08-18 20:58:23 -0600

    [UPDATE] Variable name change

--- ml-library/accessList.py
+++ ml-library/accessList.py
@@ -1,4 +1,5 @@
-my_list = [21, 44, 35, 11]
+list = [21, 44, 35, 11]
 
-for index, val in enumerate(my_list):
-    print(index, val)
+for ind, val in enumerate(list):
+    print("")
+    print(ind, val)
