3ef3d702e1b38bc657f64288cea60794a3658c5f
Agustín V <joseagustinveneveg@gmail.com>
2021-08-18 21:21:50 -0600

    JIRA-0001 [UPDATE] Do while loop instead of for loop

--- ml-library/accessList.py
+++ ml-library/accessList.py
@@ -1,5 +1,6 @@
-list = [21, 44, 35, 11]
+list = [21, 44, 35, 11, 9]
 
-for ind, val in enumerate(list):
-    print("")
-    print(ind, val)
+cont = 0
+while(cont < len(list)):
+    print(list[cont])
+    cont += 1
